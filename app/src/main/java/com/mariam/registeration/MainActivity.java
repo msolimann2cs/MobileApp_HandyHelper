db46240a7085a100e0492bd24d583b313362fb6d
mariamussama <78415993+mariamussama@users.noreply.github.com>
2023-07-09 11:43:21 +0300

    Initial Registeration

---
+++ app/src/main/java/com/mariam/registeration/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mariam.registeration;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+//login or signup
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
