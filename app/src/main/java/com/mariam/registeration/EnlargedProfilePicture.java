b281c47872b8cf8ead0c684b6be7382641ce09c1
HaruLaptop <msolimann2cs@gmail.com>
2023-07-10 06:13:54 +0300

    Added editing about me section Added indications for editing Added enlarging profile picture
    WIP
    (PROFILE TAB)
    Fixing adding categories and location
    (WALLET TAB)
    Making a ListView for the items, adding the add funds page
    (SETTINGS TAB)
    Adding the rest of the pages
    Adding log out functionality
    (GLOBAL)
    Improving the navbar

---
+++ app/src/main/java/com/mariam/registeration/EnlargedProfilePicture.java
@@ -0,0 +1,82 @@
+package com.mariam.registeration;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.imageview.ShapeableImageView;
+
+public class EnlargedProfilePicture extends AppCompatActivity {
+    private GestureDetector gestureDetector;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enlarged_profile_pic);
+
+        // Change status bar color
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(getResources().getColor(R.color.black));
+        }
+
+        ShapeableImageView fullscreenImageView = findViewById(R.id.fullscreen_image);
+        int imageResId = getIntent().getIntExtra("imageResId", 0);
+        fullscreenImageView.setImageResource(imageResId);
+
+
+
+        gestureDetector = new GestureDetector(this, new GestureDetector.SimpleOnGestureListener() {
+            @Override
+            public boolean onFling(MotionEvent event1, MotionEvent event2, float velocityX, float velocityY) {
+                // Calculate the difference in Y coordinates
+                float deltaY = event2.getY() - event1.getY();
+
+                // Check if the swipe is in an upward direction
+                if (deltaY < 0) {
+                    onBackPressed();
+                }
+
+                return true;
+            }
+        });
+
+        TextView back_button = findViewById(R.id.text_back);
+
+        back_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        gestureDetector.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
+
+//    @Override
+//    public boolean onTouchEvent(MotionEvent event) {
+//        if (event.getAction() == MotionEvent.ACTION_UP) {
+//            float deltaY = event.getRawY() - event.getY();
+//            if (deltaY < 0) {
+//                onBackPressed();
+//            }
+//        }
+//        return super.onTouchEvent(event);
+//    }
+
+
+}
--- app/src/main/java/com/mariam/registeration/ProfileMain.java
+++ app/src/main/java/com/mariam/registeration/ProfileMain.java
@@ -1,22 +1,41 @@
 package com.mariam.registeration;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
+import android.graphics.drawable.LayerDrawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.RectShape;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import java.util.concurrent.atomic.AtomicReference;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import com.google.android.material.imageview.ShapeableImageView;
 
 
 public class ProfileMain extends AppCompatActivity{
+    LinearLayout parentLayout;
+    boolean category_edit = false;
+    boolean location_edit = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_main);
+
+        LinearLayout parentLayout = findViewById(R.id.parent_layout);
 
 // Change status bar color
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
@@ -44,6 +63,132 @@ public class ProfileMain extends AppCompatActivity{
             }
         });
 
+        ShapeableImageView profile_picture = findViewById(R.id.profile_picture);
+        profile_picture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start FullscreenImageActivity and pass the image data
+                Intent intent = new Intent(ProfileMain.this, EnlargedProfilePicture.class);
+                intent.putExtra("imageResId", R.drawable.profile_photo_demo); // Pass the image resource ID or other image data
+                startActivity(intent);
+            }
+        });
+
+
+
+        Button editProfileButton = findViewById(R.id.edit_profile);
+        TextView aboutMeDescTextView = findViewById(R.id.about_me_desc);
+        AtomicReference<EditText> aboutMeDescEditTextRef = new AtomicReference<>(null);
+        LinearLayout categoriesLayout1 = findViewById(R.id.categories);
+        LinearLayout categoriesLayout2 = findViewById(R.id.categories_line2);
+        TextView locationTextView = findViewById(R.id.location);
+        editProfileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                category_edit = true;
+                location_edit = true;
+                EditText aboutMeDescEditText = aboutMeDescEditTextRef.get();
+                if (aboutMeDescEditText == null) {
+                    // Change to Edit mode
+                    aboutMeDescEditText = new EditText(ProfileMain.this);
+                    aboutMeDescEditText.setId(R.id.about_me_desc);
+                    aboutMeDescEditText.setLayoutParams(aboutMeDescTextView.getLayoutParams());
+                    aboutMeDescEditText.setText(aboutMeDescTextView.getText());
+                    aboutMeDescEditText.setSelection(aboutMeDescEditText.getText().length());
+                    aboutMeDescEditText.setTextColor(ContextCompat.getColor(ProfileMain.this, android.R.color.black));
+
+                    // Create a custom drawable for the border
+                    Drawable borderDrawable = createBorderDrawable();
+
+                    // Set the custom drawable as the background of the EditText
+                    aboutMeDescEditText.setBackground(borderDrawable);
+                    categoriesLayout1.setBackground(borderDrawable);
+                    categoriesLayout2.setBackground(borderDrawable);
+                    locationTextView.setBackground(borderDrawable);
+                    aboutMeDescEditTextRef.set(aboutMeDescEditText);
+
+                    ViewGroup parent = (ViewGroup) aboutMeDescTextView.getParent();
+                    int index = parent.indexOfChild(aboutMeDescTextView);
+                    parent.removeView(aboutMeDescTextView);
+                    parent.addView(aboutMeDescEditText, index);
+
+                    editProfileButton.setText("Done");
+                } else {
+                    // Change back to TextView mode
+                    String updatedText = aboutMeDescEditText.getText().toString();
+                    aboutMeDescTextView.setText(updatedText);
+                    aboutMeDescTextView.setTextColor(ContextCompat.getColor(ProfileMain.this, android.R.color.black));
+
+                    ViewGroup parent = (ViewGroup) aboutMeDescEditText.getParent();
+                    int index = parent.indexOfChild(aboutMeDescEditText);
+                    parent.removeView(aboutMeDescEditText);
+                    parent.addView(aboutMeDescTextView, index);
+                    categoriesLayout1.setBackground(null);
+                    categoriesLayout2.setBackground(null);
+                    locationTextView.setBackground(null);
+                    aboutMeDescEditTextRef.set(null);
+                    category_edit = false;
+                    location_edit = false;
+
+                    editProfileButton.setText("Edit Profile");
+                }
+            }
+        });
+
+
+        LinearLayout category_button1 = findViewById(R.id.categories);
+        LinearLayout category_button2 = findViewById(R.id.categories_line2);
+        category_button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LinearLayout parentLayout = findViewById(R.id.parent_layout);
+                FrameLayout overlayLayout;
+                // Start FullscreenImageActivity and pass the image data
+//                Intent intent = new Intent(ProfileMain.this, EnlargedProfilePicture.class);
+//                intent.putExtra("imageResId", R.drawable.profile_photo_demo); // Pass the image resource ID or other image data
+//                startActivity(intent);
+                overlayLayout = createOverlayLayout(); // Create the overlay layout programmatically
+                parentLayout.addView(overlayLayout);
+            }
+        });
+
+
 
     }
+
+    // Create a custom drawable for the border
+    private Drawable createBorderDrawable() {
+        int strokeWidth = getResources().getDimensionPixelSize(R.dimen.border_stroke_width);
+        int strokeColor = ContextCompat.getColor(this, R.color.off_white_darker);
+        int cornerRadius = getResources().getDimensionPixelSize(R.dimen.border_corner_radius);
+
+        GradientDrawable borderDrawable = new GradientDrawable();
+        borderDrawable.setShape(GradientDrawable.RECTANGLE);
+        borderDrawable.setStroke(strokeWidth, strokeColor);
+        borderDrawable.setCornerRadius(cornerRadius);
+
+        return borderDrawable;
+    }
+
+    private FrameLayout createOverlayLayout() {
+        //LinearLayout parentLayout = findViewById(R.id.parent_layout);
+        LayoutInflater inflater = LayoutInflater.from(this);
+        FrameLayout overlayLayout = (FrameLayout) inflater.inflate(R.layout.activity_category_edit, null);
+
+        // Customize the overlay layout and its contents programmatically if needed
+
+//        overlayLayout.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                // Handle clicks on the overlay layout (e.g., to dismiss the overlay)
+//                parentLayout.removeView(overlayLayout); // Remove the overlay layout from the parent layout
+//                // Additional handling if needed
+//            }
+//        });
+
+        return overlayLayout;
+    }
+
+
+
 }
