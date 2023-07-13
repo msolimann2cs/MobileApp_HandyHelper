ba5f46d3d5a5a672b4295e919f679cff4a8087ab
Seifeldin Elshabshiri <seifelshabshiri@aucegypt.edu>
2023-07-13 13:22:37 +0300

    updated request details to get location

--- app/src/main/java/com/mariam/registeration/RequestDetailsActivity.java
+++ app/src/main/java/com/mariam/registeration/RequestDetailsActivity.java
@@ -1,23 +1,36 @@
 package com.mariam.registeration;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.view.View;
 import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.mariam.registeration.R.id;
 import com.mariam.registeration.R.layout;
 
-public class RequestDetailsActivity extends AppCompatActivity {
+public class RequestDetailsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     TextView backBtn;
     TextView invalidText;
+    private GoogleMap myMap;
+    SupportMapFragment supportMapFragment;
 
+    double lat,lon;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,10 +43,9 @@ public class RequestDetailsActivity extends AppCompatActivity {
         TextView tvTime = (TextView)this.findViewById(id.RequestTime);
         this.backBtn = (TextView)this.findViewById(id.back);
         Button applyBtn = (Button) findViewById(id.applyBtn);
-        WebView mywebview = (WebView) findViewById(id.webViewLocation);
         EditText bidText = (EditText) findViewById(id.bidText);
+        supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(id.googlemap);
         invalidText = (TextView) findViewById(id.invalidText);
-        mywebview.getSettings().setJavaScriptEnabled(true);
         this.backBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 onBackPressed();
@@ -44,7 +56,8 @@ public class RequestDetailsActivity extends AppCompatActivity {
             tvTitle.setText(bundle.getString("reqName"));
             tvDesc.setText(bundle.getString("reqDesc"));
             tvDate.setText(bundle.getString("reqDate"));
-            mywebview.loadUrl("https://www.google.com/maps/@"+bundle.getDouble("lat")+","+bundle.getDouble("lon"));
+            lat = bundle.getDouble("lat");
+            lon= bundle.getDouble("lon");
 
             tvTime.setText(Float.toString(bundle.getFloat("reqTime")));
         }
@@ -52,22 +65,37 @@ public class RequestDetailsActivity extends AppCompatActivity {
         applyBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                int bidPrice = Integer.parseInt(bidText.getText().toString());
-                // make post request
                 boolean applied = false;
-                if(bidPrice > 10) {
-                    applied = true;
-                    Intent intent = new Intent(RequestDetailsActivity.this, AppliedActivity.class);
-                    startActivity(intent);
+                if (!(bidText.getText().toString().matches(""))) {
+                    int bidPrice = Integer.parseInt(bidText.getText().toString());
+                    // make post request
+
+                    if (bidPrice > 10) {
+                        applied = true;
+                        Intent intent = new Intent(RequestDetailsActivity.this, AppliedActivity.class);
+                        startActivity(intent);
+                    }
+
                 }
-                if(!applied){
+                if (!applied) {
                     invalidText.setVisibility(view.VISIBLE);
                 }
             }
         });
 
+        supportMapFragment.getMapAsync(RequestDetailsActivity.this);
 
 
 
     }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        myMap = googleMap;
+        LatLng location = new LatLng(lat,lon);
+        myMap.addMarker(new MarkerOptions().position(location).title("Service"));
+        myMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location, 10));
+    }
+
+
 }
